80ddd88c1a06fdd1fd8e0c0ce7463fc22414d039
Inzan Sicepat <inzanf@gmail.com>
2017-12-27 15:33:10 +0700

    update conflict

--- resi_status/models/__init__.py
+++ resi_status/models/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 from . import account_invoice
 from . import paket_bermasalah
-=======
-from . import account_invoice
 from . import account_bank
->>>>>>> a78ad50c642c091bd00d5d9167d483076d56acea
