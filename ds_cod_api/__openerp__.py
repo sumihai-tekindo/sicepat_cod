02f65818657ed261bf5f8a28cb31b38ff2b699ca
Dedi Sinaga <dedisinaga@gmail.com>
2018-01-25 08:43:36 +0700

    update openerp.py

--- ds_cod_api/__openerp__.py
+++ ds_cod_api/__openerp__.py
@@ -5,27 +5,7 @@
     'category': 'CoD',
     "summary":"""
         * login through jsonrpc post to url host/web/session/authenticate
-          pass these parameters:
-          {
-                "jsonrpc": "2.0",
-                "method": "login",
-                "params": {"db":"db_name","login":"username","password":"password"}
-
-          }
-        * to create resi:
-          post json data to url host/resi/update using these parameters:
-          headers 
-          {
-          "session_id":"session token that have been received from login",
-          "Content-Type":"application/json",
-          }
-          body
-          {
-                "jsonrpc": "2.0",
-                "method": "update_resi",
-                "params": {"resi_number":"000084119456","nik":"16110081","resi_status":"DLV"}
-          }
-
+        * update resi through host/resi/update
     """,
     'author': 'Dedi Sinaga',
     'website': '-',
